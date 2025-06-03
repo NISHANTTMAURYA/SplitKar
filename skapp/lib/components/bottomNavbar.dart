f871902a9d7cb458a0ef98c5b45179b982628032
NISHANTTMAURYA <mauryanishant2005@gmail.com>
2025-06-03 18:06:07 +0530

    bottom bar new code

--- skapp/lib/components/bottomNavbar.dart
+++ skapp/lib/components/bottomNavbar.dart
@@ -1,5 +1,190 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+class BottomNavbar extends StatefulWidget {
+  final int selectedIndex;
+  final ValueChanged<int> onItemSelected;
+  final List<String> labels;
+  final List<IconData> icons;
+
+  const BottomNavbar({
+    Key? key,
+    required this.selectedIndex,
+    required this.onItemSelected,
+    required this.labels,
+    required this.icons,
+  }) : super(key: key);
+
+  @override
+  State<BottomNavbar> createState() => _BottomNavbarState();
+}
+
+class _BottomNavbarState extends State<BottomNavbar> with SingleTickerProviderStateMixin {
+  late double itemWidth;
+  late AnimationController _controller;
+  late Animation<double> _leftAnim;
+  late Animation<double> _rightAnim;
+  int _prevIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _prevIndex = widget.selectedIndex;
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {}); // To get correct itemWidth after first build
+    });
+  }
+
+  @override
+  void didUpdateWidget(covariant BottomNavbar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.selectedIndex != _prevIndex) {
+      _startBubbleAnimation(_prevIndex, widget.selectedIndex);
+      _prevIndex = widget.selectedIndex;
+    }
+  }
+
+  void _startBubbleAnimation(int from, int to) {
+    final direction = to > from ? 1 : -1;
+    final double startLeft = from * itemWidth;
+    final double startRight = (widget.labels.length - from - 1) * itemWidth;
+    final double endLeft = to * itemWidth;
+    final double endRight = (widget.labels.length - to - 1) * itemWidth;
+    final double stretch = ((to - from).abs()) * itemWidth * 0.7;
+    if (direction > 0) {
+      // Moving right: right side moves first
+      _leftAnim = TweenSequence([
+        TweenSequenceItem(tween: Tween<double>(begin: startLeft, end: startLeft), weight: 50),
+        TweenSequenceItem(tween: Tween<double>(begin: startLeft, end: endLeft), weight: 50),
+      ]).animate(_controller);
+      _rightAnim = TweenSequence([
+        TweenSequenceItem(tween: Tween<double>(begin: startRight, end: startRight - stretch), weight: 50),
+        TweenSequenceItem(tween: Tween<double>(begin: startRight - stretch, end: endRight), weight: 50),
+      ]).animate(_controller);
+    } else {
+      // Moving left: left side moves first (no rightward movement)
+      _leftAnim = TweenSequence([
+        TweenSequenceItem(tween: Tween<double>(begin: startLeft, end: endLeft), weight: 50),
+        TweenSequenceItem(tween: Tween<double>(begin: endLeft, end: endLeft), weight: 50),
+      ]).animate(_controller);
+      _rightAnim = TweenSequence([
+        TweenSequenceItem(tween: Tween<double>(begin: startRight, end: startRight + stretch), weight: 50),
+        TweenSequenceItem(tween: Tween<double>(begin: startRight + stretch, end: endRight), weight: 50),
+      ]).animate(_controller);
+    }
+    _controller.forward(from: 0);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    itemWidth = (MediaQuery.of(context).size.width - 40) / widget.labels.length;
+    return Padding(
+      padding: EdgeInsets.only(bottom: 10 + MediaQuery.of(context).padding.bottom),
+      child: SizedBox(
+        height: 80,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Stack(
+            children: [
+              // Curved Bar Background
+              Container(
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.inversePrimary.withOpacity(0.9),
+                  // color: Colors.deepPurple[200],
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.deepPurpleAccent,
+                      blurRadius: 24,
+                      offset: Offset(0, 8),
+                    ),
+                  ],
+                ),
+              ),
+              // True Side-First Stretchy Animated Bubble
+              AnimatedBuilder(
+                animation: _controller,
+                builder: (context, child) {
+                  final left = _controller.isAnimating ? _leftAnim.value : widget.selectedIndex * itemWidth;
+                  final right = _controller.isAnimating ? _rightAnim.value : (widget.labels.length - widget.selectedIndex - 1) * itemWidth;
+                  return Positioned(
+                    left: left,
+                    right: right,
+                    top: 0,
+                    child: Container(
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.deepPurple.withOpacity(0.15),
+                            blurRadius: 8,
+                            offset: Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+              // Navigation Items
+              SizedBox(
+                height: 60,
+                child: Row(
+                  children: List.generate(widget.labels.length, (index) {
+                    return Expanded(
+                      child: GestureDetector(
+                        onTap: () => widget.onItemSelected(index),
+                        child: Container(
+                          color: Colors.transparent,
+                          height: 60,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(widget.icons[index], color: widget.selectedIndex == index ? Colors.deepPurple : Colors.grey[700], size: 28),
+                              if (widget.selectedIndex == index) ...[
+                                SizedBox(width: 8),
+                                Text(
+                                  widget.labels[index],
+                                  style: GoogleFonts.cabin(
+                                    color: Colors.deepPurple,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ]
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/*
+// ORIGINAL BottomNavbar CODE (StatelessWidget version)
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 class BottomNavbar extends StatelessWidget {
   final int selectedIndex;
   final ValueChanged<int> onItemSelected;
@@ -30,7 +215,6 @@ class BottomNavbar extends StatelessWidget {
                 height: 60,
                 decoration: BoxDecoration(
                   color: Theme.of(context).colorScheme.inversePrimary.withOpacity(0.9),
-                  // color: Colors.deepPurple[200],
                   borderRadius: BorderRadius.circular(30),
                   boxShadow: [
                     BoxShadow(
@@ -43,7 +227,7 @@ class BottomNavbar extends StatelessWidget {
               ),
               // Animated Bubble
               AnimatedPositioned(
-                duration: Duration(milliseconds: 300),
+                duration: Duration(milliseconds: 500),
                 curve: Curves.easeOutQuad,
                 left: selectedIndex * itemWidth,
                 top: 0,
@@ -103,4 +287,4 @@ class BottomNavbar extends StatelessWidget {
       ),
     );
   }
-}
+}*/
