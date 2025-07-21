7aa11e202cef2dcbc2863c7fb41a7f64d8138f3d
Nishant Maurya <mauryanishant2005@gmail.com>
2025-07-21 23:30:39 +0530

    ping

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
+    'splitkar.onrender.com',
     '10.0.2.2',
     '192.168.29.203',
     'edd3-2405-201-27-518b-5477-e6bb-9995-49d3.ngrok-free.app',
